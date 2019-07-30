a0465919f1c55ef43c4a4ab405e5fb1835c09d61
Burumbay Sanzhar <sanzharbr@gmail.com>
2019-07-30 19:54:21 +0600

    Add First and Last name page, and Gender and Birthdate selection page

---
+++ Me2/Me2/User Stories/Login Page/Views/SignUp/FirstLastNameViewController.swift
@@ -0,0 +1,19 @@
+//
+//  FirstLastNameViewController.swift
+//  Me2
+//
+//  Created by Sanzhar Burumbay on 7/30/19.
+//  Copyright © 2019 AVSoft. All rights reserved.
+//
+
+import UIKit
+
+class FirstLastNameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
