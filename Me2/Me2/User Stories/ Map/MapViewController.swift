5f2078671008f3346d1e3c264dc7fdba572cba4f
Burumbay Sanzhar <sanzharbr@gmail.com>
2019-08-02 18:22:24 +0600

    Add tab bar icons

---
+++ Me2/Me2/User Stories/ Map/MapViewController.swift
@@ -0,0 +1,18 @@
+//
+//  MapViewControllerViewController.swift
+//  Me2
+//
+//  Created by Sanzhar Burumbay on 8/2/19.
+//  Copyright © 2019 AVSoft. All rights reserved.
+//
+
+import UIKit
+
+class MapViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+}
