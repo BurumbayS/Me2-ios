26a263ff3ebede3650efeb6a07abee522339473c
Burumbay Sanzhar <sanzharbr@gmail.com>
2019-08-09 15:27:26 +0600

    Added transition to user profile

---
+++ Me2/Me2/User Stories/Chat/Views/User Profile/UserProfileHeaderTableViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  UserProfileHeaderTableViewCell.swift
+//  Me2
+//
+//  Created by Sanzhar Burumbay on 8/9/19.
+//  Copyright © 2019 AVSoft. All rights reserved.
+//
+
+import UIKit
+
+class UserProfileHeaderTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+    }
+}
---
+++ Me2/Me2/User Stories/Chat/Views/User Profile/UserProfileViewController.swift
@@ -0,0 +1,42 @@
+//
+//  UserProfileViewController.swift
+//  Me2
+//
+//  Created by Sanzhar Burumbay on 8/9/19.
+//  Copyright © 2019 AVSoft. All rights reserved.
+//
+
+import UIKit
+
+class UserProfileViewController: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        configureTableView()
+    }
+    
+    private func configureTableView() {
+        tableView.dataSource = self
+        tableView.delegate = self
+        
+        tableView.registerNib(UserProfileHeaderTableViewCell.self)
+    }
+}
+
+extension UserProfileViewController : UITableViewDelegate, UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell: UserProfileHeaderTableViewCell = tableView.dequeueReusableCell(forIndexPath: indexPath)
+        return cell
+    }
+}
