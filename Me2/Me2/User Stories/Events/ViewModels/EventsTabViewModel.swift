b368271d9b62242d7c82a62a1ceade4fea987599
Burumbay Sanzhar <sanzharbr@gmail.com>
2019-10-15 19:18:13 +0600

    Get user profile

---
+++ Me2/Me2/User Stories/Events/ViewModels/EventsTabViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  EventsTabViewModel.swift
+//  Me2
+//
+//  Created by Sanzhar Burumbay on 10/15/19.
+//  Copyright © 2019 AVSoft. All rights reserved.
+//
+
+import Foundation
+
+class EventsTabViewModel {
+    
+}
