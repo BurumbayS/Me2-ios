5d51d205e8bac160007786c1cb43275d22939ff7
Burumbay Sanzhar <sanzharbr@gmail.com>
2019-10-18 12:56:04 +0600

    Configure room info for place profile

--- Me2/Me2/User Stories/Place Profile/Views/Place/PlaceProfileHeaderCollectionViewCell.swift
+++ Me2/Me2/User Stories/Place Profile/Views/Place/PlaceProfileHeaderCollectionViewCell.swift
@@ -17,7 +17,7 @@ class PlaceProfileHeaderCollectionViewCell: UICollectionViewCell {
     let imageView = UIImageView()
     let wallpaperView = UIView()
     let imageCarousel = ImageSlideshow()
-    
+    let stackView = UIStackView()
     let logoImageView = UIImageView()
     let titleLabel = UILabel()
     let categoryLabel = UILabel()
@@ -67,6 +67,7 @@ class PlaceProfileHeaderCollectionViewCell: UICollectionViewCell {
         parentVC = viewController
         
         configureViews()
+        configureRoomInfo(with: place.roomInfo)
     }
     
     private func configureViews() {
@@ -81,6 +82,34 @@ class PlaceProfileHeaderCollectionViewCell: UICollectionViewCell {
             followButton.isHidden = true
         default:
             break
+        }
+    }
+    
+    private func configureRoomInfo(with data: RoomInfo?) {
+        guard let roomInfo = data else { return }
+        
+        let limit = (roomInfo.usersCount > 3) ? 3 : roomInfo.usersCount
+        
+        var x = 0
+        for i in 0..<limit {
+            let imageView = UIImageView(frame: CGRect(x: x, y: 0, width: 26, height: 26))
+            imageView.kf.setImage(with: URL(string: roomInfo.avatars[i]), placeholder: nil, options: [])
+            imageView.clipsToBounds = true
+            imageView.layer.cornerRadius = 13
+            
+            stackView.addSubview(imageView)
+            
+            x += 15
+        }
+        
+        if roomInfo.usersCount > 3 {
+            x += 15
+            let label = UILabel(frame: CGRect(x: x, y: 0, width: 100, height: 26))
+            label.textColor = .gray
+            label.font = UIFont(name: "Roboto-Regular", size: 13)
+            label.text = "+\(roomInfo.usersCount - 3)"
+            
+            stackView.addSubview(label)
         }
     }
     
@@ -248,6 +277,16 @@ class PlaceProfileHeaderCollectionViewCell: UICollectionViewCell {
             btn.centerY == title.centerY
             btn.width == 82
             btn.height == 40
+        }
+        
+        stackView.axis = .horizontal
+        stackView.alignment = .fill
+        view.addSubview(stackView)
+        constrain(stackView, liveChatButton) { stack, btn in
+            stack.leading == btn.leading
+            stack.trailing == btn.trailing
+            stack.height == 26
+            stack.top == btn.bottom + 10
         }
         
         self.contentView.addSubview(view)
