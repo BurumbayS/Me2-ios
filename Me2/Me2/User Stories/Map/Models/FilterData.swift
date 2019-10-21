9a3fb434e8cfbfd980a36934e2f5958f9681de00
Burumbay Sanzhar <sanzharbr@gmail.com>
2019-10-21 13:55:21 +0600

    Implement map search filters

---
+++ Me2/Me2/User Stories/Map/Models/FilterData.swift
@@ -0,0 +1,19 @@
+//
+//  FilterData.swift
+//  Me2
+//
+//  Created by Sanzhar Burumbay on 10/21/19.
+//  Copyright © 2019 AVSoft. All rights reserved.
+//
+
+import Foundation
+
+class FilterData {
+    let key: String
+    let value: Any
+    
+    init(key: String, value: Any) {
+        self.key = key
+        self.value = value
+    }
+}
