51a7142adaa7557e0f11efa03ff752c060dea32a
Burumbay Sanzhar <sanzharbr@gmail.com>
2019-11-04 11:12:43 +0600

    Add notifications page

---
+++ Me2/Me2/User Stories/Chat/Views/User Profile/Models/Notification.swift
@@ -0,0 +1,21 @@
+//
+//  Notification.swift
+//  Me2
+//
+//  Created by Sanzhar Burumbay on 11/1/19.
+//  Copyright © 2019 AVSoft. All rights reserved.
+//
+
+import Foundation
+
+class UserNotification {
+    let iconURL: String
+    let message: String
+    let time: String
+    
+    init() {
+        iconURL = ""
+        message = "Traveler's Coffee подтвердил Ваше бронирование"
+        time = "15 мин"
+    }
+}
