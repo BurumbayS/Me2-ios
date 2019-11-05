f53f0d0b88907787c954370440577864e7680a96
Burumbay Sanzhar <sanzharbr@gmail.com>
2019-11-05 13:30:54 +0600

    Store user id locally after signin

--- Me2/Me2/AppDelegate.swift
+++ Me2/Me2/AppDelegate.swift
@@ -29,11 +29,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         GIDSignIn.sharedInstance().delegate = SocialMedia.shared
         
         //Set root View Controller
-        if let _ = UserDefaults().object(forKey: UserDefaultKeys.token.rawValue) {
-            window?.rootViewController = Storyboard.mainTabsViewController()
-        } else {
-            window?.rootViewController = Storyboard.signInOrUpViewController()
-        }
+        window?.rootViewController = Storyboard.signInOrUpViewController()
+//        if let _ = UserDefaults().object(forKey: UserDefaultKeys.token.rawValue) {
+//            window?.rootViewController = Storyboard.mainTabsViewController()
+//        } else {
+//            window?.rootViewController = Storyboard.signInOrUpViewController()
+//        }
         
         //Configure IQKeyboard
         IQKeyboardManager.shared.enable = true
