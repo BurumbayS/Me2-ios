6d88e9167611ec678561ee4d006549df9289b57c
Burumbay Sanzhar <sanzharbr@gmail.com>
2019-11-11 12:22:35 +0600

    Configure about app page

--- Me2/Me2/User Stories/Chat/Views/User Profile/Views/Additional pages/AboutAppViewController.swift
+++ Me2/Me2/User Stories/Chat/Views/User Profile/Views/Additional pages/AboutAppViewController.swift
@@ -12,11 +12,20 @@ class AboutAppViewController: UIViewController {
 
     @IBOutlet weak var navBar: UINavigationBar!
     @IBOutlet weak var navItem: UINavigationItem!
+    @IBOutlet weak var versionLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         configureNavBar()
+        configureData()
+    }
+    
+    private func configureData() {
+        if let appVersion = Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String {
+            versionLabel.text = "Версия \(appVersion)"
+        }
     }
     
     private func configureNavBar() {
@@ -24,5 +33,11 @@ class AboutAppViewController: UIViewController {
         setUpBackBarButton(for: navItem)
         
         navItem.title = "О приложении"
+    }
+    
+    @IBAction func showPrivacyPolicy(_ sender: Any) {
+    }
+    
+    @IBAction func rateApp(_ sender: Any) {
     }
 }
