b635128fe0e95786b858cd51991df24dc9a68754
Burumbay Sanzhar <sanzharbr@gmail.com>
2019-11-17 00:14:54 +0600

    Some changhes

---
+++ Me2/Me2/User Stories/Chat/Views/User Profile/ViewModels/AddFavouritePlaceViewModel.swift
@@ -0,0 +1,101 @@
+//
+//  AddFavouritePlaceViewModel.swift
+//  Me2
+//
+//  Created by Sanzhar Burumbay on 11/16/19.
+//  Copyright © 2019 AVSoft. All rights reserved.
+//
+
+import Alamofire
+import SwiftyJSON
+
+class AddFavouritePlaceViewModel {
+    var currentFavouritePlaces: [Place]
+    var addPlacehandler: ((Place) -> ())?
+    var searchResults = [Place]()
+    let searchValue: Dynamic<String>
+    let updateSearchResults: Dynamic<Bool>
+    
+    init(favouritePlaces: [Place], onAddPlace: ((Place) -> ())?) {
+        self.currentFavouritePlaces = favouritePlaces
+        self.addPlacehandler = onAddPlace
+        
+        self.updateSearchResults = Dynamic(false)
+        self.searchValue = Dynamic("")
+        
+        bindDynamics()
+    }
+    
+    private func bindDynamics() {
+        self.searchValue.bind { [unowned self] (value) in
+            if value != "" {
+                
+                DispatchQueue.main.asyncAfter(deadline: .now() + 0.5, execute: {
+                    if self.searchValue.value == value {
+                        self.searchPlace(by: value)
+                    }
+                })
+                
+            } else {
+                
+                self.searchResults = []
+                self.updateSearchResults.value = true
+                
+            }
+        }
+    }
+    
+    private func searchPlace(by searchValue: String) {
+        let url = placesURL + "?search=\(searchValue)"
+        let encodedUrl = url.addingPercentEncoding(withAllowedCharacters: NSCharacterSet.urlQueryAllowed)!
+        
+        Alamofire.request(encodedUrl, method: .get, parameters: nil, encoding: JSONEncoding.default, headers: Network.getHeaders())
+            .responseJSON { (response) in
+                switch response.result {
+                case .success(let value):
+                    
+                    let json = JSON(value)
+                    
+                    self.searchResults = []
+                    for item in json["data"]["results"].arrayValue {
+                        let place = Place(json: item)
+                        self.searchResults.append(place)
+                    }
+                    
+                    self.updateSearchResults.value = true
+                    
+                case .failure(let error):
+                    print(error.localizedDescription)
+                }
+        }
+    }
+    
+    func addPlaceToFavourite(with id: Int, completion: ResponseBlock?) {
+        let url = placesURL + "\(id)/add_favourite/"
+        
+        Alamofire.request(url, method: .get, parameters: nil, encoding: JSONEncoding.default, headers: Network.getAuthorizedHeaders())
+            .responseJSON { (response) in
+                switch response.result {
+                case .success(let value):
+                    
+                    let json = JSON(value)
+                    let code = json["code"].intValue
+                    
+                    switch code {
+                    case 0:
+                        completion?(.ok, "")
+                    default:
+                        completion?(.error, json["message"].stringValue)
+                    }
+                    
+                    self.updateSearchResults.value = true
+                    
+                case .failure(let error):
+                    print(error.localizedDescription)
+                    completion?(.fail, "")
+                }
+        }
+    }
+    
+     private let placesURL = Network.core + "/place/"
+}
